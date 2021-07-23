c5a062da4caf51253a5d45017b975d7041e503d0
rtae <potae02@gmail.com>
2021-07-23 18:59:57 +0700

    fix : bug

--- PPEDetection/src/controllers/model_controller.py
+++ PPEDetection/src/controllers/model_controller.py
@@ -1,5 +1,7 @@
 from src.queue.tasks import inference
+from src.services.model_service import model
 
 def controller_inference(image_byte):
-    result = inference(image_byte)
+    image_str = model().imgByte2imgStr(image_byte)
+    result = inference.delay(image_str)
     return result
